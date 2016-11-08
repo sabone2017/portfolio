e1593405d88a436487a68beed138ee93143d832f
Sean <sabone3192@gmail.com>
2016-11-07 20:55:10 -0500

    revamping html and buttoncode to prepare for insertion of slider

--- Portfolio/buttoncode.js
+++ Portfolio/buttoncode.js
@@ -1,13 +1,14 @@
 var enterButton = document.getElementById("enter");
-enterButton.onclick = function showPage(){
+//enterButton.onclick = showPage();
+
+function showPage(){
   var theBody = document.getElementsByTagName("body")[0];
   var header = document.getElementsByClassName("headBorder")[0];
   var sectOne = document.getElementById("intro");
   var sectTwo = document.getElementById("midBox");
   header.style.display = "block";
-  sectOne.style.height = "800px";
+  sectOne.style.display= "none";
   theBody.style.backgroundImage = "url(assets/images/bodyPattern.png)"
   theBody.style.backgroundSize = "cover";
-
   sectTwo.style.display = "block";
 }
