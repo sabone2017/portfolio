ca37c7c6a118429aef3b0b2b146e63c6b7c0c44d
Sean Bone <sabone3192@gmail.com>
2016-11-14 09:01:24 -0500

    use caps for file ending for linux

--- SliderJS/js/slideScript.js
+++ SliderJS/js/slideScript.js
@@ -8,8 +8,6 @@ setPreview();
 function incSlide(n){
   slideIndex +=n;
   showSlide(slideIndex);
-  document.getElementsByClassName('preview-active')[0].classList.remove('preview-active');
-  document.getElementsByClassName('preview')[slideIndex-1].classList.add('preview-active');
 }
 
 function showSlide(n){
@@ -30,10 +28,8 @@ function setPreview(){
   //used to test global var: alert(slides[slideIndex].src);
   i = 0;
   for(i ; i<slides.length ; i++){
-    //prevIMG[i].style.height = (document.getElementsByClassName('slideContainer')[0].style.height / slides.length) + "px";
     var newPrev = document.createElement('img');
     newPrev.src = slides[i].src;
-    newPrev.draggable = "false";
     prevIMG[i].appendChild(newPrev);
     prevIndex[i] = prevIMG[i];
     prevIMG[i].addEventListener('click' , activePreview);
@@ -49,7 +45,7 @@ function setPreview(){
 function activePreview(n){
   var activePrev = document.getElementsByClassName('preview-active')[0];
   //change the class to preview
-  // for older browsers to remove class names?? no idea : activePrev.className = activePrev.className.replace(/(?:^|\s)preview-acitve/ , 'preview');
+  // for older browsersl to remove class names?? no idea : activePrev.className = activePrev.className.replace(/(?:^|\s)preview-acitve/ , 'preview');
   activePrev.classList.remove('preview-active');
   this.className = "preview preview-active";
   var i = 0;
