8764437044d49ab3d30eed69e202bf42702c8272
Sean <sabone3192@gmail.com>
2016-11-16 03:33:13 -0500

    CREATED FOLDER FOR IMAGE BOARD TESTING
    Using imageBoard folder for testing of the image board design
    Created board and boxes in new folder
    Ignore new js file as it is not needed to do the hover
    Created hover functionality to show overlay in css
    Created description-area div to house full images
    Check psd for design changes

---
+++ imageBoard/js/boardScript.js
@@ -0,0 +1,41 @@
+/*Going to make this thing work
+- fill the boxes with the correct images
+- set the hover action to make the grayed background over the top of the boxe
+- set the info area to have the correct image
+- show the information in a text box in the info div
+And that should be it*/
+var boxes = document.getElementsByClassName('imageBox');
+//var shades = document.getElementsByClassName('boxHover');
+//on hover action
+/* get the imagebox elements
+  show div over element with correct background and transparency
+  do not use a loop to collect all hoverBoxes it will do them all at once
+    make a function to relate the image box to the hover box using QuerySelector
+*/
+hoverView();
+function hoverView(){
+  var i = 0;
+  for(i ; i<boxes.length ; i++){
+    boxes[i].addEventListener("mouseenter" , hoverIn);
+    boxes[i].addEventListener("mouseleave" , hoverOut);
+  }
+/*  boxes[0].onmouseenter = function shadeBox(){
+    for(i=0 ; i<shades.length ; i++){
+      shades[i].style.display = "block";
+    }
+  };
+  boxes[0].onmouseleave = function unShadeBox(){
+    for(i=0 ; i<shades.length ; i++){
+      shades[i].style.display = "none";
+    }
+  };*/
+}
+
+function hoverIn(){
+  var shade = this.querySelector('.boxHover');
+  shade.style.display = "block";
+}
+function hoverOut(){
+  var shade = this.querySelector('.boxHover');
+  shade.style.display = "none";
+}
