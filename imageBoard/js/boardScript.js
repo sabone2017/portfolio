94072ecee3c32c32305d0fb085c67a85e6053045
Sean <sabone3192@gmail.com>
2016-11-17 15:22:33 -0500

    WORKING ON THE BACK GROUND IMAGE FOR THE IMAGEBOARD
    setting up the image to slide into background on hover
    work is still being done in test file this is probably what git is
    supposed to be for but whatever instead of making a test folder
    maybe ill branch it to implement it branch as uxrework-ib or some shit

--- imageBoard/js/boardScript.js
+++ imageBoard/js/boardScript.js
@@ -1,17 +1,8 @@
-/*Going to make this thing work
-- fill the boxes with the correct images
-- set the hover action to make the grayed background over the top of the boxe
-- set the info area to have the correct image
-- show the information in a text box in the info div
-And that should be it*/
 var boxes = document.getElementsByClassName('imageBox');
-//var shades = document.getElementsByClassName('boxHover');
-//on hover action
-/* get the imagebox elements
-  show div over element with correct background and transparency
-  do not use a loop to collect all hoverBoxes it will do them all at once
-    make a function to relate the image box to the hover box using QuerySelector
-*/
+/*Need to get the boxes to select proper preview image
+  Make it so it is only affected by a current box
+  Might need to make an array for the images to be selected from*/
+var fullImageList = [];
 hoverView();
 function hoverView(){
   var i = 0;
@@ -32,13 +23,47 @@ function hoverView(){
 }
 
 function hoverIn(){
-  var description = document.getElementById('description-area');
-  var descImg = description.getElementsByTagName('img')[0];
+  //var description = document.getElementById('description-area');
+  var descImg = document.getElementsByClassName('descImg')[0];
   descImg.classList.add('activeImg')
 }
 
 function hoverOut(){
-  var description = document.getElementById('description-area');
-  var descImg = description.getElementsByTagName('img')[0];
+  //var description = document.getElementById('description-area');
+  var descImg = document.getElementsByClassName('descImg')[0];
   descImg.classList.remove('activeImg')
 }
+
+//array for images possibly make it in html and just select by DOM selectors
+//option 2: create an image object and create an array of image objects
+// each object will have two properties of type (preview or full) and url(actual link to image)
+function imageMaker(type , link){
+  //Error Catches for incorrect typed image properties
+  if(typeof type != 'string'){
+    console.log('Invalid Type for Image Board Picture');
+  }
+  if(typeof link != 'string'){
+    console.log('Invalid URL inserted for Image Board Picture');
+  }
+  if(!(type==="preview") && !(type==="full")){
+      console.log('Invalid Type for Image Board Picture');
+  }
+  //property setters
+  this.imageType = type;
+  this.imageURL = link;
+  //push to correct array based on type of image
+  if(this.imageType == "full"){
+    fullImageList.push(this);
+    console.log(fullImageList);
+  }
+}
+function setPreview(){
+  var descImg = document.getElementsByClassName('descImg')[0];
+  descImg.src = fullImageList[0];
+}
+var textTime = new imageMaker("full" , "../images/textTimeFull2.png");
+var carbon = new imageMaker("full" , "../images/CarbonFull.png");
+var holdera = new imageMaker('full' , '../images/holder.png');
+var holderb = new imageMaker('full' , '../images/holder.png');
+var holderc = new imageMaker('full' , '../images/holder.png');
+var holderd = new imageMaker('full' , '../images/holder.png');
